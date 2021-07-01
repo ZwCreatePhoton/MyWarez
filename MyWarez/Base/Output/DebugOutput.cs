d8903832eee4ed44011cc22070e903fdea49584c
ZwCreatePhoton <ZwCreatePhoton@gmail.com>
2021-06-30 23:34:27 -0700

    Added DebugOutput

---
+++ MyWarez/Base/Output/DebugOutput.cs
@@ -0,0 +1,18 @@
+﻿using MyWarez.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace MyWarez.Base
+{
+    public class DebugOutput : FileOutput
+    {
+        public static string OutputDirectoryName = "Debug";
+
+        public DebugOutput(string baseDirectory = "")
+            : base(Path.Join(baseDirectory, OutputDirectoryName))
+        { }
+    }
+}
